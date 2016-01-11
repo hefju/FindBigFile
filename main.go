c44dfeabada11a1654634e8823866dc063611810
hefju <hefju@qq.com>
2016-01-11 23:06:07 +0800

    还有个学习的网址

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func visit(path string, fi os.FileInfo, err error) error {
 func main(){
 
     //参考一下这个网址, 去掉重复文件的.http://golanghome.com/post/476
+    //还有一个网址:http://golangtc.com/t/54571b25421aa960c7000027
     root := "E:/111" //"H:/winxp"
     err := filepath.Walk(root, visit)
     fmt.Printf("filepath.Walk() returned %v\n", err)
