2ac7502fed7402bd4e0b4f3c36f07af76e679369
hefju <hefju@qq.com>
2016-01-18 11:25:40 +0800

    输出大文件时候, 路径path其实包含文件名了. 不用再次输出文件名了.

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func visit(path string, fi os.FileInfo, err error) error {
         return nil
     }
 
-    fmt.Printf("Visited: %s\n", path)
+   // fmt.Printf("Visited: %s\n", path)
     if fi.Mode().IsRegular() {
-        fmt.Println(fi.Name(), fi.Size(), "bytes")
+        fmt.Println("Visited:"+path, fi.Size(), "bytes")//fi.Name()
     }
     return nil
 }
@@ -27,7 +27,7 @@ func main(){
 
     //参考一下这个网址, 去掉重复文件的.http://golanghome.com/post/476
     //还有一个网址:http://golangtc.com/t/54571b25421aa960c7000027
-    root := "c:/" //"H:/winxp"
+    root := "d:/" //"H:/winxp"
     err := filepath.Walk(root, visit)
     fmt.Printf("filepath.Walk() returned %v\n", err)
 
